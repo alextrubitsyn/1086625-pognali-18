7583cb16557de9c374843515a5f68e39aaf1e1a9
Alex <9970421@gmail.com>
2019-12-03 18:22:59 +0300

    Исправляет ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
+var noJS = document.querySelector(".nojs");
 var headerEl = document.querySelector(".page-header");
 var toggleEl = document.querySelector(".page-header__toggle");
 
-headerEl.classList.remove("page-header--nojs");
+noJS.classList.remove("nojs");
 
 toggleEl.onclick = function() {
   headerEl.classList.toggle("page-header--menu-open");
