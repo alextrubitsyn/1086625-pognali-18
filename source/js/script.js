0104bdaf06c5e5890b9e6e36edf0eb5b18856f6e
Alex <9970421@gmail.com>
2020-01-04 22:56:15 +0300

    Добавляет стилизацию form

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,15 @@
 var noJS = document.querySelector(".nojs");
 var headerEl = document.querySelector(".page-header");
 var toggleEl = document.querySelector(".page-header__toggle");
+var countryFormEl = document.querySelector(".country-filter--form");
+var toggleFormEl = document.querySelector(".country__btn--null");
 
 noJS.classList.remove("nojs");
 
 toggleEl.onclick = function() {
   headerEl.classList.toggle("page-header--menu-open");
+};
+
+toggleFormEl.onclick = function() {
+  countryFormEl.classList.toggle("country-filter--open");
 };
