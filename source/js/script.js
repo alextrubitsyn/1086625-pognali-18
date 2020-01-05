44f62e3d14c998784f6fb1dd2f0e8aa3833e7193
Alex <9970421@gmail.com>
2020-01-05 23:00:24 +0300

    Исправляет ошибки БЭМ в contacts и добавляет открытие меню выбора стран

--- source/js/script.js
+++ source/js/script.js
@@ -4,18 +4,33 @@ var toggleEl = document.querySelector(".page-header__toggle");
 var countryFormEl = document.querySelector(".country-filter--form");
 var toggleFormEl = document.querySelector(".country__btn--null");
 var countryCatalogEl = document.querySelector(".country-filter--catalog");
-var toggleCatalogEl = document.querySelector(".country-filter__toggle");
+var toggleCatalog1El = document.querySelector(".country-filter__toggle");
+var toggleCatalog2El = document.querySelector(".country-filter__close");
 
-noJS.classList.remove("nojs");
+if (noJS) {
+  noJS.classList.remove("nojs");
+}
 
-toggleEl.onclick = function() {
-  headerEl.classList.toggle("page-header--menu-open");
-};
+if (toggleEl) {
+  toggleEl.onclick = function() {
+    headerEl.classList.toggle("page-header--menu-open");
+  };
+}
 
-toggleFormEl.onclick = function() {
-  countryFormEl.classList.toggle("country-filter--open");
-};
+if (toggleFormEl) {
+  toggleFormEl.onclick = function() {
+    countryFormEl.classList.toggle("country-filter--open");
+  };
+}
 
-toggleCatalogEl.onclick = function() {
-  countryCatalogEl.classList.toggle("country-filter--open");
-};
+if (toggleCatalog1El) {
+  toggleCatalog1El.onclick = function() {
+    countryCatalogEl.classList.toggle("country-filter--open");
+  };
+}
+
+if (toggleCatalog2El) {
+  toggleCatalog2El.onclick = function() {
+    countryCatalogEl.classList.toggle("country-filter--open");
+  };
+}
