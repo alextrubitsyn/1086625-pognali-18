73065f18f0ec3e010872783a6bd97e67f502ed38
Alex <9970421@gmail.com>
2020-01-06 20:41:06 +0300

    Добавляет скролл и модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -6,6 +6,10 @@ var toggleFormEl = document.querySelector(".country__btn--null");
 var countryCatalogEl = document.querySelector(".country-filter--catalog");
 var toggleCatalog1El = document.querySelector(".country-filter__toggle");
 var toggleCatalog2El = document.querySelector(".country-filter__close");
+var businessRatesEl = document.querySelector(".business-rates");
+var modalOverlayEl = document.querySelector(".modal-overlay");
+var modalOpenEl = document.querySelector(".add-profile__business-link");
+var modalCloseEl = document.querySelector(".business-rates__close");
 
 if (noJS) {
   noJS.classList.remove("nojs");
@@ -33,4 +37,60 @@ if (toggleCatalog2El) {
   toggleCatalog2El.onclick = function() {
     countryCatalogEl.classList.toggle("country-filter--open");
   };
+}
+
+if (modalOpenEl && modalCloseEl && modalOverlayEl && businessRatesEl) {
+  modalOpenEl.addEventListener("click", openModal);
+  modalCloseEl.addEventListener("click", closeModal);
+  modalOverlayEl.addEventListener("click", closeModal);
+
+  function openModal(event) {
+    event.preventDefault();
+
+    modalOverlayEl.classList.add("modal-overlay--open");
+    businessRatesEl.classList.add("business-rates--open");
+  }
+
+  function closeModal(event) {
+    if (event && event.preventDefault) {
+      event.preventDefault();
+    }
+
+    modalOverlayEl.classList.remove("modal-overlay--open");
+    businessRatesEl.classList.remove("business-rates--open");
+  }
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      event.preventDefault();
+
+      if (businessRatesEl.classList.contains("business-rates--open")) {
+        closeModal();
+      }
+    }
+  });
+}
+
+if (headerEl) {
+  var headerFixed = false;
+
+  window.addEventListener("scroll", headerOnScroll);
+
+  function headerOnScroll() {
+    if (document.documentElement.scrollTop > 0) {
+      if (headerFixed) {
+        return;
+      }
+
+      headerEl.classList.add("page-header--scroll");
+      headerFixed = true;
+    } else {
+      if (!headerFixed) {
+        return;
+      }
+
+      headerEl.classList.remove("page-header--scroll");
+      headerFixed = false;
+    }
+  }
 }
